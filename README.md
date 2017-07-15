cfe216f5baeece8e195511862cd7a854dfaafac3
Jorden Chamid <info@chamidmedia.nl>
2017-07-15 15:34:45 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Slide smart curtains for Athom Homey
+
+Control your curtains with Slide using the Homey by Athom B.V.
+
+**Want to show your appreciation for this app? A donation is possible via http://www.d2c.nl **
+
+You first need to have the IP-address of your Slide box. Make sure this is a fixed IP address, or otherwise the device cannot be controlled. You can only control it if it is in the same network as Homey.
+
+Enables the following actions to use in your flows:
+
+
+Use at your own risk, I accept no responsibility for any damages caused by using this script.
+
+# Changelog
+
+**Version 0.1.0:**
+- First release
---
+++ drivers/slide/driver.js
@@ -0,0 +1,172 @@
+"use strict";
+
+var request = require('request');
+var tempIP = '';
+var devices = {};
+
+module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj, changedKeysArr, callback ) {
+
+    Homey.log ('Changed settings: ' + JSON.stringify(device_data) + ' / ' + JSON.stringify(newSettingsObj) + ' / old = ' + JSON.stringify(oldSettingsObj));
+    
+    try {
+      changedKeysArr.forEach(function (key) {
+        devices[device_data.id].settings[key] = newSettingsObj[key]
+      })
+      callback(null, true)
+    } catch (error) {
+      callback(error)
+    }
+
+};
+
+module.exports.init = function(devices_data, callback) {
+    
+    devices_data.forEach(function initdevice(device) {
+	    
+	    Homey.log('add device: ' + JSON.stringify(device));
+	    
+	    devices[device.id] = device;    
+	    
+	    module.exports.getSettings(device, function(err, settings){
+		    devices[device.id].settings = settings;
+		});
+		
+	});
+	
+	Homey.log("Slide app - init done");
+	
+	callback (null, true);
+};
+
+module.exports.deleted = function( device_data ) {
+    
+    Homey.log('deleted: ' + JSON.stringify(device_data));
+    
+    devices[device_data.id] = [];
+	
+};
+
+module.exports.pair = function (socket) {
+	// socket is a direct channel to the front-end
+
+	// this method is run when Homey.emit('list_devices') is run on the front-end
+	// which happens when you use the template `list_devices`
+	socket.on('list_devices', function (data, callback) {
+
+		Homey.log("Slide app - list_devices tempIP is " + tempIP);
+		
+		var devices = [{
+			name				: tempIP,
+			data: {
+				id				: tempIP,
+			},
+			settings: {
+				"ipaddress" 	: tempIP
+			}
+		}];
+
+		callback (null, devices);
+
+	});
+
+	// this is called when the user presses save settings button in start.html
+	socket.on('get_devices', function (data, callback) {
+
+		// Set passed pair settings in variables
+		tempIP = data.ipaddress;
+		Homey.log ( "Slide app - got get_devices from front-end, tempIP =" + tempIP );
+
+		// assume IP is OK and continue
+		socket.emit ('continue', null);
+
+	});
+
+	socket.on('disconnect', function(){
+		Homey.log("Slide app - User aborted pairing, or pairing is finished");
+	})
+}
+
+// CAPABILITIES
+module.exports.capabilities = {
+    onoff: {
+
+        get: function( device_data, callback ){
+
+			if (typeof devices[device_data.id] === "undefined") {
+				
+				callback (null, false);
+				
+			} else {
+			
+				Homey.log('Getting device_status of ' + devices[device_data.id].settings.ipaddress);
+	            
+	            						
+            
+            }
+        },
+
+        set: function( device_data, turnon, callback ) {
+	        
+	        Homey.log('Setting device_status of ' + devices[device_data.id].settings.ipaddress + ' to ' + turnon);
+
+			if (turnon) {
+				
+				var r = request.post("http://" + devices[device_data.id].settings.ipaddress + "/api/v1.0/slide/open", requestCallback);
+				
+				function requestCallback(err, res, body) {
+				  	Homey.log(body);
+				  	if (body.success == true) callback (null, true);
+				}
+				
+			} else {
+				
+				var r = request.post("http://" + devices[device_data.id].settings.ipaddress + "/api/v1.0/slide/close", requestCallback);
+				
+				function requestCallback(err, res, body) {
+				  	Homey.log(body);
+				  	if (body.success == true) callback (null, true);
+				}
+				
+			}
+
+        }
+    }
+}
+
+Homey.on('unload', function(){
+	
+});
+
+// flow action handlers
+Homey.manager('flow').on('action.OpenSlide', function (callback, args) {
+
+	var r = request.post("http://" + devices[device_data.id].settings.ipaddress + "/api/v1.0/slide/open", requestCallback);
+				
+	function requestCallback(err, res, body) {
+	  	Homey.log(body);
+	  	if (body.success == true) callback (null, true);
+	}
+				
+});
+
+Homey.manager('flow').on('action.CloseSlide', function (callback, args) {
+	
+	var r = request.post("http://" + devices[device_data.id].settings.ipaddress + "/api/v1.0/slide/close", requestCallback);
+				
+	function requestCallback(err, res, body) {
+	  	Homey.log(body);
+	  	if (body.success == true) callback (null, true);
+	}
+
+});
+
+Homey.manager('flow').on('action.ImmediateStop', function (callback, args) {
+	
+	var r = request.post("http://" + devices[device_data.id].settings.ipaddress + "/api/v1.0/slide/stop", requestCallback);
+				
+	function requestCallback(err, res, body) {
+	  	Homey.log(body);
+	  	if (body.success == true) callback (null, true);
+	}
+
+});
