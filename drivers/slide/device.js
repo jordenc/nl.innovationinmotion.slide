225edec84080a71672f8110610cce7c21853cc8e
Jorden Chamid <info@chamidmedia.nl>
2017-12-28 12:41:00 +0100

    Work in progress

--- drivers/slide/device.js
+++ drivers/slide/device.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Homey = require('homey');
+let request	= require('request');
 
 class SlideDevice extends Homey.Device {
 
@@ -11,66 +12,26 @@ class SlideDevice extends Homey.Device {
         this.log('class:', this.getClass());
 
         // register a capability listener
-        this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this))
-        this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this))
+        this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this));
         
-    }
+        var device_data = this.getData();
 
-    // this method is called when the Device is added
-    onAdded() {
-        this.log('device added');
-    }
-
-    // this method is called when the Device is deleted
-    onDeleted() {
-        this.log('device deleted');
-    }
-
-    // this method is called when the Device has requested a state change (turned on or off)
-    onCapabilityOnoff( value, opts, callback ) {
-
-        // ... set value to real device
-        this.log ("ONOFF value = " + JSON.stringify (value));
-        
-        // Then, emit a callback ( err, result )
-        callback( null );
-
-        // or, return a Promise
-        return Promise.reject( new Error('Switching the device failed!') );
-    }
-    
-    onCapabilityDim (value, callback) {
-	    
-	 	this.log ("DIM value = " + JSON.stringify (value));
-	 	
-	 	if (value > 0 || value > 1) {
-		 	
-		 	return Promise.reject( new Error('Value must be between 0.00 and 1.00 (' + value + ')') );
-		 	
-	 	} else {
-	 	
-	 		var requestData = {"pos": value}
-	 		
-	 		var url = 'http://hostname.local/rpc/Slide.SetPos';
-	 		
-	    	/*
-		    	request({
-				    url: url,
-				    method: "POST",
-				    json: requestData
+		let ImmediateStopAction = new Homey.FlowCardAction('ImmediateStop');
+		ImmediateStopAction
+			.register()
+			.registerRunListener((args, state) => {
+				
+				request({
+					url: 'http://' + device_data.host + "/rpc/Slide.Stop",
+					method: "GET"
 				},
 				function (error, response, body) {
+			        
+			        var result = JSON.parse(body);
+			            
 			        if (!error && response.statusCode === 200) {
 			            
-			            if (body.response == "success") {
-				         
-				         	callback (null, true);
-				            
-				        } else {
-					     
-					     	callback (body.response, false);
-					        
-					    }
+			            return Promise.resolve(true);
 			            
 			        }
 			        else {
@@ -80,9 +41,104 @@ class SlideDevice extends Homey.Device {
 			            console.log("response.statusText: " + response.statusText)
 			        }
 			    })
-			*/
-			
-			callback (null);
+				
+			});
+        
+        /* ERROR: this.setCapabilityValue is not a function
+        request({
+			url: 'http://' + device_data.host + "/rpc/Slide.GetInfo",
+			method: "GET"
+		},
+		function (error, response, body) {
+	        
+	        var result = JSON.parse(body);
+	            
+	        if (!error && response.statusCode === 200) {
+	            
+	            if (result.pos < 0) result.pos = 0;
+	            
+	            this.setCapabilityValue ("dim", result.pos);
+	            
+	        }
+	        else {
+	
+	            console.log("error: " + error)
+	            console.log("response.statusCode: " + response.statusCode)
+	            console.log("response.statusText: " + response.statusText)
+	        }
+	    })
+        */
+        
+    }
+
+    // this method is called when the Device is added
+    onAdded() {
+        this.log('device added');
+        
+        var device_data = this.getData();
+
+		if (device_data.pos < 0) device_data.pos = 0;
+        this.setCapabilityValue ("dim", device_data.pos);
+        
+    }
+
+    // this method is called when the Device is deleted
+    onDeleted() {
+        this.log('device deleted');
+    }
+
+    onCapabilityDim (value, opts, callback) {
+	    
+	    this.log("opts = " + JSON.stringify (opts));
+	    
+	 	this.log("callback = " + JSON.stringify (callback));
+	    
+	    this.log ("DIM value = " + JSON.stringify (value));
+	 	
+	 	if (value < 0 || value > 1) {
+		 	
+		 	return Promise.reject( new Error('Value must be between 0.00 and 1.00 (' + value + ')') );
+		 	
+	 	} else {
+	 	
+	 		var requestData = {"pos":  value}
+	 		var device_data = this.getData();
+	 		
+	 		//var requestData = '{"pos":  "' + value + '"}';
+	 		
+	 		this.log("requestData = " + requestData);
+	 		
+	    	request({
+			    url: 'http://' + device_data.host + '/rpc/Slide.SetPos',
+			    method: "POST",
+			    json: requestData
+			},
+			function (error, response, body) {
+		        if (!error && response.statusCode === 200) {
+		            
+		            console.log("error: " + error)
+		            console.log("response.statusCode: " + response.statusCode)
+		            console.log("response.statusText: " + response.statusText)
+		            console.log("body = " + JSON.stringify (body));
+		            
+		            if (body.response == "success") {
+			         
+			         	callback (null, true);
+			            
+			        } else {
+				     
+				     	callback (body.response, false);
+				        
+				    }
+		            
+		        }
+		        else {
+		
+		            console.log("error: " + error)
+		            console.log("response.statusCode: " + response.statusCode)
+		            console.log("response.statusText: " + response.statusText)
+		        }
+		    })
 			
 	    }
 	    
--- drivers/slide/driver.js
+++ drivers/slide/driver.js
@@ -3,20 +3,68 @@
 'use strict';
 
 const Homey = require('homey');
+let mdns 	= require('node-mdns-js');
+let request	= require('request');
+let devices = [];
 
 class SlideDriver extends Homey.Driver {
 
     onPairListDevices( data, callback ){
 
-        callback( null, [
-            {
-                name: 'Foo Device',
-                data: {
-                    id: 'foo'
-                }
-            }
-        ]);
-
+	    var browser = mdns.createBrowser(mdns.tcp('slide'));
+	    
+	    browser.on('ready', function onReady() {
+		  console.log('browser is ready');
+		  browser.discover();
+		});
+		
+		
+		browser.on('update', function onUpdate(data) {
+		console.log('data:', data);
+		  
+		request({
+			url: 'http://' + data.host + "/rpc/Slide.GetInfo",
+			method: "GET"
+		},
+			function (error, response, body) {
+		        
+		        var result = JSON.parse(body);
+		            
+		        if (!error && response.statusCode === 200) {
+		            
+		             var device = {
+						  name: result.device_name,
+						  data: {
+							  id: data.fullname,
+							  ip: data.addresses,
+							  mac: result.mac,
+							  host: data.host,
+							  curtain_type: result.curtain_type,
+							  pos: result.pos
+						  }
+					  }
+					  
+					  devices.push(device);
+		            
+		        }
+		        else {
+		
+		            console.log("error: " + error)
+		            console.log("response.statusCode: " + response.statusCode)
+		            console.log("response.statusText: " + response.statusText)
+		        }
+		    })
+		  
+		});
+		
+		//stop after timeout
+		setTimeout(function onTimeout() {
+			browser.stop();
+			console.log ("--TIME OUT--");
+			console.log ("DEVICES = " + JSON.stringify (devices));
+			callback (null, devices);
+		}, 5000);
+	    
     }
 
 }
