aad96086437b087f31b29ec38b88d62822fe1373
Jorden Chamid <jchamid@totallyhosted.nl>
2018-02-26 11:06:04 +0100

    Remove non-slide devices while detecting

--- drivers/slide/driver.js
+++ drivers/slide/driver.js
@@ -18,41 +18,49 @@ class SlideDriver extends Homey.Driver {
 		
 		
 		browser.on('update', function onUpdate(data) {
-		console.log('data:', data);
-		  
-		request({
-			url: 'http://' + data.host + "/rpc/Slide.GetInfo",
-			method: "GET"
-		},
-			function (error, response, body) {
-		        
-		        var result = JSON.parse(body);
-		            
-		        if (!error && response.statusCode === 200) {
-		            
-		             var device = {
-						  name: result.device_name,
-						  data: {
-							  id: data.fullname,
-							  ip: data.addresses,
-							  mac: result.mac,
-							  host: data.host,
-							  curtain_type: result.curtain_type,
-							  pos: result.pos
-						  }
-					  }
-					  
-					  devices.push(device);
-		            
-		        }
-		        else {
-		
-		            console.log("error: " + error)
-		            console.log("response.statusCode: " + response.statusCode)
-		            console.log("response.statusText: " + response.statusText)
-		        }
-		    })
-		  
+			
+			console.log('data:', data);
+
+			if (data.fullname.substr(0, 5) == "slide") {
+				  
+				request({
+					url: 'http://' + data.host + "/rpc/Slide.GetInfo",
+					method: "GET"
+				},
+					function (error, response, body) {
+				        
+				        var result = JSON.parse(body);
+				            
+				        if (!error && response.statusCode === 200) {
+				            
+				             var device = {
+								  name: result.device_name,
+								  data: {
+									  id: data.fullname,
+									  ip: data.addresses,
+									  mac: result.mac,
+									  host: data.host,
+									  curtain_type: result.curtain_type,
+									  pos: result.pos
+								  }
+							  }
+							  
+							  devices.push(device);
+				            
+				        }
+				        else {
+				
+				            console.log("error: " + error)
+				            console.log("response.statusCode: " + response.statusCode)
+				            console.log("response.statusText: " + response.statusText)
+				        }
+				    })
+		    } else {
+			    
+			    console.log (data.fullname + " is not a Slide device");
+			    
+		    }
+		    
 		});
 		
 		//stop after timeout
