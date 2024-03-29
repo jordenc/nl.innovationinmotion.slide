d98a4af041b86e2bf9489eadf801c8a8b097dfae
Jorden Chamid <info@corbata.nl>
2024-03-29 10:49:05 +0100

    Slide detection via MDNS

--- drivers/slide_local/driver.js
+++ drivers/slide_local/driver.js
@@ -1,10 +1,8 @@
 "use strict";
 
 const Homey = require('homey');
-const SlideAuth = require('../../include/slideauth');
-const SlideHousehold = require('../../include/slidehousehold');
 
-class SlideDriver extends Homey.Driver {
+class SlideLocalDriver extends Homey.Driver {
 
 	/**
 	 *
@@ -12,45 +10,28 @@ class SlideDriver extends Homey.Driver {
 	 */
 	async onPair(session) {
 
-		let username = "";
-		let password = "";
-
 		await session.showView("set_local");
 
 		session.setHandler('list_devices', async () => {
 
-			//const api = await DeviceAPI.login({ username, password });
-			//const api = await SlideDriver.login({ username, password });
+			this.log('list_devices searching mDNS');
 
-			var devices = [];
+			const discoveryStrategy = this.getDiscoveryStrategy('mdns-sd');
+			const discoveryResults = discoveryStrategy.getDiscoveryResults();
 
-			return slideHouseHold.getOverview().then(result => {
+			const devices = Object.values(discoveryResults).map(discoveryResult => {
 
-				console.log(result.slides);
+				this.log('discoveryResult', discoveryResult);
 
-				result.slides.forEach(function (device) {
-					devices.push({
-						data: {
-							id: device.device_id,
-							numid: device.id,
-							name: device.device_name,
-							calib_time: 30000,
-							slide_setup: device.slide_setup,
-							household_id: device.household_id,
-							zone_id: device.zone_id,
-							touch_go: device.touch_go,
-							pos: device.device_info.pos,
-						},
-						name: device.device_name
-					});
-				});
-
-				return devices;
-
-			}).catch(err => {
-				this.log(err);
-				return false;
+				return {
+					name: discoveryResult.txt.name,
+					data: {
+						id: discoveryResult.id,
+					},
+				};
 			});
+
+			return devices;
 
 		});
 	}
@@ -59,51 +40,9 @@ class SlideDriver extends Homey.Driver {
 	 * Driver initialisation done
 	 */
 	async onInit() {
-		this.log("Driver initialisation done");
-		this.timer = setInterval(this.checkToken.bind(this), 86400000);
-		this.checkToken();
-	}
-
-	/**
-	 * Checks if access token is still valid
-	 */
-	async checkToken() {
-
-		var expires = this.homey.settings.get('token_expires');
-		var expire_date = new Date(expires);
-		var expireDate = expire_date.getTime();
-
-		var date = new Date();
-		var renew_date = date.getDate() + 14;	//Renew token after 14 days (30 days validity)
-		date.setDate(renew_date);
-		var newDate = date.getTime();
-
-		if (newDate > expireDate) {
-
-			var username = this.homey.settings.get('username');
-			var password = this.homey.settings.get('password');
-
-			if (username && password) {
-
-				var slideAuth = new SlideAuth();
-				slideAuth.login(username, password).then(result => {
-
-					this.homey.settings.set('token', result.access_token);
-					this.homey.settings.set('token_expires', result.expires_at)
-
-				}).catch(err => {
-					this.log(err);
-				});
-
-			} else {
-				this.log("No username and password setup yet");
-			}
-		} else {
-			this.log("Token is still valid for more than 14 days");
-		}
-
+		this.log("Local driver initialisation done");
 	}
 
 }
 	  
-module.exports = SlideDriver;
+module.exports = SlideLocalDriver;
